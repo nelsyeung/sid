3c37e51ab1fb7c3bf9bcf1e7408384c11b005ca8
Nelson Yeung <nelsyeung@icloud.com>
2018-09-08 22:57:32 +0100

    Refactor out the neural network to nn.py

---
+++ sid/nn.py
@@ -0,0 +1,77 @@
+from tensorflow.keras.layers import (
+    Conv2D,
+    Conv2DTranspose,
+    Input,
+    Lambda,
+    MaxPooling2D,
+    concatenate,
+)
+from tensorflow.keras.models import Model
+
+from sid import metric
+
+
+def model(height, width, channels, summary=False):
+    """Returns a U-Net neural network model using Keras.
+
+    Args:
+        height (int): Input image height.
+        width (int): Input image width.
+        channels (int): Input image number of channels.
+        summary (bool): Whether to print a summary model.
+
+    Returns:
+        Model: The Keras neural network model.
+    """
+    inputs = Input((height, width, channels))
+    s = Lambda(lambda x: x / 255)(inputs)
+
+    c1 = Conv2D(8, (3, 3), activation='relu', padding='same')(s)
+    c1 = Conv2D(8, (3, 3), activation='relu', padding='same')(c1)
+    p1 = MaxPooling2D((2, 2))(c1)
+
+    c2 = Conv2D(16, (3, 3), activation='relu', padding='same')(p1)
+    c2 = Conv2D(16, (3, 3), activation='relu', padding='same')(c2)
+    p2 = MaxPooling2D((2, 2))(c2)
+
+    c3 = Conv2D(32, (3, 3), activation='relu', padding='same')(p2)
+    c3 = Conv2D(32, (3, 3), activation='relu', padding='same')(c3)
+    p3 = MaxPooling2D((2, 2))(c3)
+
+    c4 = Conv2D(64, (3, 3), activation='relu', padding='same')(p3)
+    c4 = Conv2D(64, (3, 3), activation='relu', padding='same')(c4)
+    p4 = MaxPooling2D(pool_size=(2, 2))(c4)
+
+    c5 = Conv2D(128, (3, 3), activation='relu', padding='same')(p4)
+    c5 = Conv2D(128, (3, 3), activation='relu', padding='same')(c5)
+
+    u6 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(64, (3, 3), activation='relu', padding='same')(u6)
+    c6 = Conv2D(64, (3, 3), activation='relu', padding='same')(c6)
+
+    u7 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(32, (3, 3), activation='relu', padding='same')(u7)
+    c7 = Conv2D(32, (3, 3), activation='relu', padding='same')(c7)
+
+    u8 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(16, (3, 3), activation='relu', padding='same')(u8)
+    c8 = Conv2D(16, (3, 3), activation='relu', padding='same')(c8)
+
+    u9 = Conv2DTranspose(8, (2, 2), strides=(2, 2), padding='same')(c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(8, (3, 3), activation='relu', padding='same')(u9)
+    c9 = Conv2D(8, (3, 3), activation='relu', padding='same')(c9)
+
+    outputs = Conv2D(1, (1, 1), activation='sigmoid')(c9)
+
+    model = Model(inputs=[inputs], outputs=[outputs])
+    model.compile(optimizer='adam', loss='binary_crossentropy',
+                  metrics=[metric.mean_iou])
+
+    if summary:
+        model.summary()
+
+    return model
--- train.py
+++ train.py
@@ -5,21 +5,11 @@ from tensorflow.keras.callbacks import (
     EarlyStopping,
     ModelCheckpoint,
 )
-from tensorflow.keras.layers import (
-    Conv2D,
-    Conv2DTranspose,
-    Input,
-    Lambda,
-    MaxPooling2D,
-    concatenate,
-)
-from tensorflow.keras.models import Model
 from tqdm import tqdm
 import numpy as np
 import os
 
-from sid import metric
-
+from sid import nn
 
 path_train = os.path.join('input', 'train')
 path_test = os.path.join('input', 'test')
@@ -48,56 +38,7 @@ for n, id in tqdm(enumerate(train_ids), total=len(train_ids)):
 
 print('Done!')
 
-# Build U-Net model
-inputs = Input((im_height, im_width, 1))
-s = Lambda(lambda x: x / 255)(inputs)
-
-c1 = Conv2D(8, (3, 3), activation='relu', padding='same')(s)
-c1 = Conv2D(8, (3, 3), activation='relu', padding='same')(c1)
-p1 = MaxPooling2D((2, 2))(c1)
-
-c2 = Conv2D(16, (3, 3), activation='relu', padding='same')(p1)
-c2 = Conv2D(16, (3, 3), activation='relu', padding='same')(c2)
-p2 = MaxPooling2D((2, 2))(c2)
-
-c3 = Conv2D(32, (3, 3), activation='relu', padding='same')(p2)
-c3 = Conv2D(32, (3, 3), activation='relu', padding='same')(c3)
-p3 = MaxPooling2D((2, 2))(c3)
-
-c4 = Conv2D(64, (3, 3), activation='relu', padding='same')(p3)
-c4 = Conv2D(64, (3, 3), activation='relu', padding='same')(c4)
-p4 = MaxPooling2D(pool_size=(2, 2))(c4)
-
-c5 = Conv2D(128, (3, 3), activation='relu', padding='same')(p4)
-c5 = Conv2D(128, (3, 3), activation='relu', padding='same')(c5)
-
-u6 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c5)
-u6 = concatenate([u6, c4])
-c6 = Conv2D(64, (3, 3), activation='relu', padding='same')(u6)
-c6 = Conv2D(64, (3, 3), activation='relu', padding='same')(c6)
-
-u7 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c6)
-u7 = concatenate([u7, c3])
-c7 = Conv2D(32, (3, 3), activation='relu', padding='same')(u7)
-c7 = Conv2D(32, (3, 3), activation='relu', padding='same')(c7)
-
-u8 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c7)
-u8 = concatenate([u8, c2])
-c8 = Conv2D(16, (3, 3), activation='relu', padding='same')(u8)
-c8 = Conv2D(16, (3, 3), activation='relu', padding='same')(c8)
-
-u9 = Conv2DTranspose(8, (2, 2), strides=(2, 2), padding='same')(c8)
-u9 = concatenate([u9, c1], axis=3)
-c9 = Conv2D(8, (3, 3), activation='relu', padding='same')(u9)
-c9 = Conv2D(8, (3, 3), activation='relu', padding='same')(c9)
-
-outputs = Conv2D(1, (1, 1), activation='sigmoid')(c9)
-
-model = Model(inputs=[inputs], outputs=[outputs])
-model.compile(optimizer='adam', loss='binary_crossentropy',
-              metrics=[metric.mean_iou])
-model.summary()
-
+model = nn.model(im_height, im_width, im_chan)
 earlystopper = EarlyStopping(patience=5, verbose=1)
 checkpointer = ModelCheckpoint('model-sid.h5', verbose=1,
                                save_best_only=True)
