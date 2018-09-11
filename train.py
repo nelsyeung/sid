2346f7b4a06d9405c437856ce933254d5c469e4c
Nelson Yeung <nelsyeung@icloud.com>
2018-09-11 18:39:20 +0100

    Update train with more data per epoch
    - Giving the highest score yet of 0.690.
    
    - 8 times the training set (3600 * 8) and validation set (400 * 8).
    
    - The epoch is increased to 60 since we are using an early stopper anyway.

--- train.py
+++ train.py
@@ -24,11 +24,11 @@ x, y, _ = utils.get_data(path_train, width, height, channels, True,
 model = nn.model(width, height, channels)
 
 datagen_args = dict(
-    rotation_range=20,
+    rotation_range=10,
     width_shift_range=0.5,
     height_shift_range=0.5,
     zoom_range=0.5,
-    shear_range=0.2,
+    shear_range=0.1,
     horizontal_flip=True,
     vertical_flip=True,
     validation_split=0.1,
@@ -47,8 +47,8 @@ gen_validation = zip(
 earlystopper = EarlyStopping(patience=5, verbose=1)
 checkpointer = ModelCheckpoint('model.h5', verbose=1, save_best_only=True)
 model.fit_generator(gen_train,
-                    epochs=30,
-                    steps_per_epoch=(len(x) * 0.9) // batch_size,
+                    epochs=60,
+                    steps_per_epoch=(len(x) * 0.9),
                     validation_data=gen_validation,
-                    validation_steps=(len(x) * 0.1) // batch_size,
+                    validation_steps=(len(x) * 0.1),
                     callbacks=[earlystopper, checkpointer])
